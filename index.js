b773704ca432d79ba12b15ed05b93df592ea5768
RamiRbehi <75276419+RamiRbehi@users.noreply.github.com>
2021-02-05 23:22:24 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+$(".images-list").hide();
+
+$(".btn").click(function(){
+  if ( $( ".images-list" ).first().is( ":hidden" ) ) {
+   $( ".images-list" ).toggle( "slow" );
+ } else {
+   $( ".images-list" ).hide("slow");
+ }
+});
+});
+
+$(document).ready(function() {
+$(".navbar-toggler").click(function (event) {
+  $(".new-bg, .list, .burger, .close").toggleClass("active");
+});
+});
